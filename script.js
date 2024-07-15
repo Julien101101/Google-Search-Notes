0718c1fea13f849c6090868b1cf05a7ffdfcd5b4
Julien Savary <juliensavary2001@gmail.com>
2024-07-15 16:08:24 -0700

    added index, json, script files

---
+++ script.js
@@ -0,0 +1,3 @@
+// this script will connect To the premit.com website and sync the bookmark notifications to there
+// moreover the script will allow the bookmarks of the browser to be taken notes of. This will link
+
